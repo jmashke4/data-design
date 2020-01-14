8d728fc6f3b13a4931baff1a1209cc129c90632f
Josh Mashke <jmashke@cnm.edu>
2020-01-14 10:37:10 -0700

    created ul

--- epic/persona.php
+++ epic/persona.php
@@ -9,9 +9,12 @@
 		<img src="BingeWatcher.jpg" alt="Binge">
 		<p>Gender: Female</p>
 		<p>Age: 23</p>
-		<p>Occupation: </p>
+		<p>Occupation: College student</p>
 		<p>Technology: Iphone X, Macbook Pro, Apple TV</p>
 		<p>Attitude: likes to be caught up in today's Pop Culture, likes giving movie and tv show recommendations Considers herself to be the most culturally educated amongst her friends</p>
-		<p></p>
+		<p>Needs/Goals/Features:</p>
+			<ul>
+				<li></li>
+			</ul>
 	</body>
 </html>
