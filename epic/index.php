7239d498c98ff7373c827f9c448b25b919ce628c
Josh Mashke <jmashke@cnm.edu>
2020-01-14 10:08:45 -0700

    created html files

---
+++ epic/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Data Design</title>
+	</head>
+	<body>
+	<h1>Data Design Project</h1>
+	<a href="persona.php">Persona</a>
+	<a href="user-story.php">User Story</a>
+	<a href="use-case-interaction.php">Use Case Interaction</a>
+	</body>
+
+</html>
