5180be2ae0af98898bed1eb0677cf71fc20b3568
Josh Mashke <jmashke@cnm.edu>
2020-01-14 15:40:18 -0700

    fixed the h3,4,5

--- epic/use-case-interaction.php
+++ epic/use-case-interaction.php
@@ -8,13 +8,13 @@
 		<h1>Use Case:</h1>
 		<h2>Title: </h2>
 		<p>Adding The Witcher to her watchlist</p>
-		<h3>Description:</h3>
+		<h2>Description:</h2>
 		<p>Bailey is about to finish Game of Thrones and is looking for a new show to fill the massively disappointing void the final season left,
 			when one of her friends recommends The Witcher. Since Bailey is not done with Game of Thrones yet, she wants to add The Witcher to her
 			watchlist so she can come back to it later.</p>
-		<h4>Precondition:</h4>
+		<h2>Precondition:</h2>
 		<p>Bailey is logged into her Netflix account</p>
-		<h5>Postcondition:</h5>
+		<h2>Postcondition:</h2>
 		<p>The Witcher is saved for later</p>
 
 		<ul>
