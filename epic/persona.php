b90e75b88b689897b8f8f858e253a1ec8634d327
Josh Mashke <jmashke@cnm.edu>
2020-01-14 10:21:27 -0700

    added image

--- epic/persona.php
+++ epic/persona.php
@@ -5,6 +5,10 @@
 		<title>Persona</title>
 	</head>
 	<body>
+		<h1>Name: Bailey Binge</h1>
+		<img src="BingeWatcher.jpg" alt="Binge">
+		<p>Gender: Female</p>
+		<p>Age: 23</p>
 
 	</body>
 </html>
