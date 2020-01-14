7600b2395dfaff90ea7ad24e42cca41839997357
Josh Mashke <jmashke@cnm.edu>
2020-01-14 16:02:55 -0700

    fixed the index page

--- epic/index.php
+++ epic/index.php
@@ -6,9 +6,9 @@
 	</head>
 	<body>
 	<h1>Data Design Project</h1>
-	<a href="persona.php">Persona</a>
-	<a href="user-story.php">User Story</a>
-	<a href="use-case-interaction.php">Use Case Interaction</a>
+	<p><a href="persona.php">Persona</a></p>
+	<p><a href="user-story.php">User Story</a></p>
+	<p><a href="use-case-interaction.php">Use Case Interaction</a></p>
 	</body>
 
 </html>
