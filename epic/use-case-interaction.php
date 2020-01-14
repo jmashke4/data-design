fb20827e4c42fb803e65869b0ffd9f94ab52cb50
Josh Mashke <jmashke@cnm.edu>
2020-01-14 15:38:11 -0700

    added h3, h4, and ptags

--- epic/use-case-interaction.php
+++ epic/use-case-interaction.php
@@ -12,6 +12,10 @@
 		<p>Bailey is about to finish Game of Thrones and is looking for a new show to fill the massively disappointing void the final season left,
 			when one of her friends recommends The Witcher. Since Bailey is not done with Game of Thrones yet, she wants to add The Witcher to her
 			watchlist so she can come back to it later.</p>
+		<h4>Precondition:</h4>
+		<p>Bailey is logged into her Netflix account</p>
+		<h5>Postcondition:</h5>
+		<p>The Witcher is saved for later</p>
 
 		<ul>
 			<li>Bailey enters netflix.com into her browser</li>
