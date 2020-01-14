24ebe3a62e03a3f67c179c4eab1b63b58e111329
Josh Mashke <jmashke@cnm.edu>
2020-01-14 16:24:16 -0700

    added link to styles2.css

--- epic/index.php
+++ epic/index.php
@@ -5,6 +5,7 @@
 		<title>Data Design</title>
 	</head>
 	<body>
+	<link rel="stylesheet" href="styles2.css">
 	<h1>Data Design Project</h1>
 	<p><a href="persona.php">Persona</a></p>
 	<p><a href="user-story.php">User Story</a></p>
