de7e5ef8542830843517de9f5b5e0b6cd0651034
Josh Mashke <jmashke@cnm.edu>
2020-01-14 10:32:59 -0700

    add p tags

--- epic/persona.php
+++ epic/persona.php
@@ -9,6 +9,9 @@
 		<img src="BingeWatcher.jpg" alt="Binge">
 		<p>Gender: Female</p>
 		<p>Age: 23</p>
-
+		<p>Occupation: </p>
+		<p>Technology: Iphone X, Macbook Pro, Apple TV</p>
+		<p>Attitude: likes to be caught up in today's Pop Culture, likes giving movie and tv show recommendations Considers herself to be the most culturally educated amongst her friends</p>
+		<p></p>
 	</body>
 </html>
