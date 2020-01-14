10e4976bfaaca40e38f3cdc5a5ef93a862097ca2
Josh Mashke <jmashke@cnm.edu>
2020-01-14 10:47:28 -0700

    created html for user story

--- epic/user-story.php
+++ epic/user-story.php
@@ -1,1 +1,11 @@
-<?php
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>User Story</title>
+	</head>
+	<body>
+	<p>As a user, Bailey would like to stream her favorite TV show</p>
+	</body>
+
+</html>
