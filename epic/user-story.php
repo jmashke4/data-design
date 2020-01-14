afb8fb5edf739d49e2b1cf4c4f4ff839b1c1c430
Josh Mashke <jmashke@cnm.edu>
2020-01-14 16:40:55 -0700

    changed background color

--- epic/user-story.php
+++ epic/user-story.php
@@ -5,6 +5,7 @@
 		<title>User Story</title>
 	</head>
 	<body>
+	<link rel="stylesheet" href="styles.css">
 	<h1>User Story:</h1>
 	<p>As a user, Bailey would like to stream her favorite TV shows.</p>
 	</body>
