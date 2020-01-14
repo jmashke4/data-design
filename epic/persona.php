c15f71d9b5ba703ee09d79cbf233e22dcd06bbb6
Josh Mashke <jmashke@cnm.edu>
2020-01-14 15:52:53 -0700

    added bold characters

--- epic/persona.php
+++ epic/persona.php
@@ -5,20 +5,35 @@
 		<title>Persona</title>
 	</head>
 	<body>
-		<h1>Name: Bailey Binge</h1>
+		<h1>Name: Bailey Binger</h1>
 		<img src="BingeWatcher.jpg" alt="Binge">
-		<p>Gender: Female</p>
-		<p>Age: 23</p>
-		<p>Occupation: College student</p>
-		<p>Technology: Iphone X, Macbook Pro, Apple TV</p>
-		<p>Attitude: likes to be caught up in today's Pop Culture, likes giving movie and tv show recommendations Considers herself to be the most culturally educated amongst her friends</p>
-		<p>Needs/Goals/Features:</p>
+		<p>
+			<strong>Gender:</strong> Female
+		</p>
+		<p>
+			<strong>Age:</strong> 23
+		</p>
+		<p>
+			<strong>Occupation:</strong> College student
+		</p>
+		<p>
+			<strong>Technology:</strong> Iphone X, Macbook Pro, Apple TV
+		</p>
+		<p>
+			<strong>Attitude: </strong>likes to be caught up in today's Pop Culture, likes giving movie and tv show recommendations,
+			Considers herself to be the most culturally educated amongst her friends
+		</p>
+		<p>
+			<strong>Needs/Goals/Features:</strong>
+		</p>
 			<ul>
 				<li>Ability to watch her favorite movies and tv shows</li>
 				<li>Receive tailored recommendations based off her viewing history</li>
 				<li>Leave reviews</li>
 			</ul>
-		<p>Frustrations</p>
+		<p>
+			<strong>Frustrations</strong>
+		</p>
 			<ul>
 				<li>Hates commercials</li>
 				<li>Doesn't want to pay the ridiculous amount for cable</li>
