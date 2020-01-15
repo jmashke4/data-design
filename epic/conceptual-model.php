7be77d8dd861e3e399944c405258f489e31275ec
Josh Mashke <jmashke@cnm.edu>
2020-01-15 14:26:10 -0700

    added colons to the h2s

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -7,27 +7,28 @@
 	<body>
 	<link rel="stylesheet" href="styles.css">
 	<h1>Entities and Attributes</h1>
-	<h2><strong>Profile</strong></h2>
+	<h2><strong>Profile:</strong></h2>
 		<ul>
 			<li>User ID</li>
-			<li>First and Last name</li>F
+			<li>First and Last name</li>
 			<li>Credit Card</li>
 			<li>Rental History</li>
 			<li>Email</li>
 		</ul>
-	<h2><strong>Movie</strong></h2>
+	<h2><strong>Movie:</strong></h2>
 		<ul>
-			<li>Movie_ID</li>
+			<li>Movie ID</li>
 			<li>Average Rating</li>
 			<li>Year</li>
 			<li>Starring</li>
 			<li>Summary</li>
 		</ul>
-	<h2><strong>Rating</strong></h2>
+	<h2><strong>Rating:</strong></h2>
 		<ul>
 			<li>Rating User ID</li>
 			<li>Rating Movie Id</li>
 		</ul>
+	<h2>ERD:</h2>
 	<img src="erd.png" alt="erd">
 	</body>
 </html>
