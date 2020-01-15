a5f760cdd45d3ca62db0f93427f26d42a388077d
Josh Mashke <jmashke@cnm.edu>
2020-01-15 13:29:47 -0700

    added h1,h2, and ul tags

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -5,6 +5,18 @@
 		<title>Phase 2</title>
 	</head>
 	<body>
-	
+	<h1>Entities and Attributes</h1>
+	<h2><strong>Profile</strong></h2>
+		<ul>
+			<li></li>
+		</ul>
+	<h2><strong>Movie</strong></h2>
+		<ul>
+			<li></li>
+		</ul>
+	<h2><strong>Watchlist</strong></h2>
+		<ul>
+			<li></li>
+		</ul>
 	</body>
 </html>
