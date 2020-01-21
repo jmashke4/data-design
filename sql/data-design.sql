dba098364720607fb5acd286e78b8aa7438c1074
Josh Mashke <jmashke@cnm.edu>
2020-01-21 09:58:35 -0700

    added tables to MySQL

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,3 +1,38 @@
-create table test(
-	testId binary(16) not null,
+DROP TABLE IF EXISTS `Rating`;
+DROP TABLE IF EXISTS movie;
+DROP TABLE IF EXISTS profile;
+
+
+CREATE TABLE profile (
+
+	profileId BINARY(16) NOT NULL,
+	profileFirstName CHAR(32),
+	profileLastName VARCHAR(32) NOT NULL,
+	profileEmail VARCHAR(128) NOT NULL,
+	profileCreditCard CHAR(97) NOT NULL,
+	profileWatchHistory VARCHAR(32),
+	UNIQUE(profileEmail),
+	PRIMARY KEY(profileId)
+);
+
+
+CREATE TABLE movie (
+	movieId BINARY(16) NOT NULL,
+	movieRating BINARY(16) NOT NULL,
+	movieSummary VARCHAR(140) NOT NULL,
+	movieYear DATETIME(6) NOT NULL,
+	INDEX(movieId),
+	FOREIGN KEY(movieId) REFERENCES profile(profileId),
+	PRIMARY KEY(movieId)
+);
+
+CREATE TABLE `rating` (
+	ratingProfileId BINARY(16) NOT NULL,
+	ratingMovieId BINARY(16) NOT NULL,
+	likeDate DATETIME(6) NOT NULL,
+	INDEX(ratingProfileId),
+	INDEX(ratingMovieId),
+	FOREIGN KEY(ratingProfileId) REFERENCES profile(profileId),
+	FOREIGN KEY(ratingMovieId) REFERENCES movie(movieId),
+	PRIMARY KEY(ratingProfileId, ratingMovieId)
 );
